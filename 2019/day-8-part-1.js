bec3c3d19049ff3cdb1cbc4f1aa66a65a2316765
Michael Stewart <mistewart@linkedin.com>
2020-02-18 17:05:02 -0800

    basic idea, just need some details

--- 2019/day-8-part-1.js
+++ 2019/day-8-part-1.js
@@ -5,3 +5,33 @@
 const fs = require('fs');
 
 const INPUT_FILE = './day-8-input.txt';
+const IMAGE_WIDTH = 25;
+const IMAGE_HEIGHT = 6;
+//const IMAGE_WIDTH = 3;
+//const IMAGE_HEIGHT = 2;
+
+// TODO:
+function countDigits(pixelData) {
+}
+
+// read the file as a string
+let imageData = fs.readFileSync(INPUT_FILE, "utf-8").trim();
+//let imageData = '123456789012';
+
+let numberOfPixels = IMAGE_WIDTH * IMAGE_HEIGHT;
+let currentLayer = 1;
+let layerWithFewestZeros = 0;
+let layerInfo = {};
+
+while (imageData.length > 0) {
+  // for each row, get the pixel data
+  let [pixelData, remainingData] = [imageData.substring(0, numberOfPixels), imageData.slice(numberOfPixels)]
+  //console.log(`layer ${currentLayer}:`);
+  //console.log(pixelData);
+  layerInfo[currentLayer] = { data: pixelData, digits: countDigits(pixelData) };
+  // TODO: track least zeros
+
+  // keep going
+  imageData = remainingData;
+  currentLayer++;
+}
