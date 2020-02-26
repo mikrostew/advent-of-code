ddfffe1f1e28502b5acd84919806da5e5fac766f
Michael Stewart <mikrostew@gmail.com>
2020-02-25 22:24:37 -0800

    putting stuff on the screen

--- 2019/day-13-part-1.js
+++ 2019/day-13-part-1.js
@@ -9,6 +9,13 @@ const IntcodeProgram = require('./intcode-program');
 
 const INPUT_FILE = './day-13-input.txt';
 
+const TILES = {
+  0: ' ', // empty
+  1: '█', // wall
+  2: '□', // block
+  3: '—', // horizontal paddle
+  4: 'O', // ball
+}
 
 // arcade thing
 class ArcadeCabinet {
@@ -21,9 +28,21 @@ class ArcadeCabinet {
     let programStr = fs.readFileSync(file, "utf-8").trim();
     return new ArcadeCabinet(programStr);
   }
+
+  tileIdToChar(tileID) {
+    return TILES[tileID];
+  }
+
+  putTileAtPosition(x, y, tileID) {
+    // TODO
+    process.stdout.write(`\x1b[${x};${y}H${this.tileIdToChar(tileID)}`);
+  }
 }
 
 
 // input the program and run it
-//map = AsteroidMap.fromFile(INPUT_FILE);
-//map.findBestLocation();
+let arcade = ArcadeCabinet.fromFile(INPUT_FILE);
+arcade.putTileAtPosition(1, 7, 1);
+arcade.putTileAtPosition(1, 8, 2);
+arcade.putTileAtPosition(1, 9, 3);
+arcade.putTileAtPosition(1, 11, 4);
