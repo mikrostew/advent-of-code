679f70045901a0864ac57bba002255946ddc6a53
Michael Stewart <mikrostew@gmail.com>
2022-12-29 15:47:45 -0500

    use seq! macro to simplify things

--- 2022/src/days/mod.rs
+++ 2022/src/days/mod.rs
@@ -5,33 +5,13 @@ use nom::combinator::opt;
 use nom::combinator::recognize;
 use nom::sequence::tuple;
 use nom::IResult;
+use seq_macro::seq;
 
-// TODO: can I use seq! macro here?
-pub mod day1;
-pub mod day10;
-pub mod day11;
-pub mod day12;
-pub mod day13;
-pub mod day14;
-pub mod day15;
-pub mod day16;
-pub mod day17;
-pub mod day18;
-pub mod day19;
-pub mod day2;
-pub mod day20;
-pub mod day21;
-pub mod day22;
-pub mod day23;
-pub mod day24;
-pub mod day25;
-pub mod day3;
-pub mod day4;
-pub mod day5;
-pub mod day6;
-pub mod day7;
-pub mod day8;
-pub mod day9;
+seq!(N in 1..=25 {
+    pub mod day~N;
+});
+
+// TODO: split this stuff into a utils crate or something?
 
 // because I do this all the time
 // (not deriving Copy because some structs use String)
--- 2022/src/main.rs
+++ 2022/src/main.rs
@@ -1,6 +1,7 @@
 use std::fs;
 
 use clap::Parser;
+use seq_macro::seq;
 
 mod cli;
 mod days;
@@ -20,35 +21,14 @@ fn main() {
     println!("Day {}", args.day);
 
     // validate days and parts
-    // TODO: can I use seq! macro for this?
-    let day_fn: fn(String, Option<cli::Params>) -> String = match args.day {
-        1 => runner_fn_for_day!(day1, args.part),
-        2 => runner_fn_for_day!(day2, args.part),
-        3 => runner_fn_for_day!(day3, args.part),
-        4 => runner_fn_for_day!(day4, args.part),
-        5 => runner_fn_for_day!(day5, args.part),
-        6 => runner_fn_for_day!(day6, args.part),
-        7 => runner_fn_for_day!(day7, args.part),
-        8 => runner_fn_for_day!(day8, args.part),
-        9 => runner_fn_for_day!(day9, args.part),
-        10 => runner_fn_for_day!(day10, args.part),
-        11 => runner_fn_for_day!(day11, args.part),
-        12 => runner_fn_for_day!(day12, args.part),
-        13 => runner_fn_for_day!(day13, args.part),
-        14 => runner_fn_for_day!(day14, args.part),
-        15 => runner_fn_for_day!(day15, args.part),
-        16 => runner_fn_for_day!(day16, args.part),
-        17 => runner_fn_for_day!(day17, args.part),
-        18 => runner_fn_for_day!(day18, args.part),
-        19 => runner_fn_for_day!(day19, args.part),
-        20 => runner_fn_for_day!(day20, args.part),
-        21 => runner_fn_for_day!(day21, args.part),
-        22 => runner_fn_for_day!(day22, args.part),
-        23 => runner_fn_for_day!(day23, args.part),
-        24 => runner_fn_for_day!(day24, args.part),
-        25 => runner_fn_for_day!(day25, args.part),
-        _ => panic!("Day {} is out of range", args.day),
-    };
+    seq!(N in 1..=25 {
+        let day_fn: fn(String, Option<cli::Params>) -> String = match args.day {
+            #(
+                N => runner_fn_for_day!(day~N, args.part),
+            )*
+            _ => panic!("Day {} is out of range", args.day),
+        };
+    });
 
     // input params
     let params = match args.params {
